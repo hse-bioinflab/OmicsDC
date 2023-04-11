543f9c3360681328b677b7d7fe3e9944e28e1b80
MrARVO <voytetskiyartem@gmail.com>
2023-04-11 19:54:52 +0300

    modified:   data/omicDC.py 	modified:   data/omicDC.py

--- data/omicDC.py
+++ data/omicDC.py
@@ -136,7 +136,6 @@ def omics(expid: str = None, assembly: str = 'hg38', assembly_threshold: str = '
 
     # move exp file
     if not os.path.isfile(FILE_PATH + f"/experimentList.tab"):
-        os.replace("./data/storage/experimentList.tab.gz", FILE_PATH)
         os.system(f"gunzip {FILE_PATH}/experimentList.tab.gz")
 
     if not os.path.isfile(FILE_PATH + f"/allPeaks_light.{assembly}.{assembly_threshold}.bed"):
@@ -170,7 +169,7 @@ def omics(expid: str = None, assembly: str = 'hg38', assembly_threshold: str = '
                 print(f"{bcolors.OKCYAN}U r not alone. Sorry but u have to w8.\nChill a bit!{bcolors.ENDC}") 
                 exit()
 
-    match_exp_df = create_matching_expirement_df("experimentList.tab", options)
+    match_exp_df = create_matching_expirement_df(FILE_PATH + "experimentList.tab", options)
 
     create_sorted_bed_file(que, f"allPeaks_light.{assembly}.{assembly_threshold}.bed", match_exp_df)
     que.shutdown()
