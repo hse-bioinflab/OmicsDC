37d396ca3730131511c52a631e707e1b0daec1ff
Xbostick <borovkov-pavel@yandex.ru>
2023-05-08 14:47:01 +0300

    Fixing bugs and user experience 	modified:   data/loader.ipynb 	modified:   data/loader/file_creator.py 	modified:   data/loader/omicDC.py

--- data/loader/omicDC.py
+++ data/loader/omicDC.py
@@ -1,33 +1,28 @@
 from pathlib import Path
 from typing import Union
+from time import gmtime, strftime
 
 import subprocess
 import pandas as pd
 import numpy as np
-import dask.dataframe as dd
-from dask.distributed import Client
-import wget
 
 import multiprocessing
 import os
-import warnings
+import subprocess
 
 def create_matching_expirement_df(
             filepath,
-            options,
-            verbose
-        ):
+            options
+        ) -> pd.DataFrame:
     """ Function to return expirement names df"""
 
     # match_exp_df - df for matching experiments
     match_exp_df = pd.read_csv(
                     filepath,
-                    sep = '\t', 
-                    names = ['id', 'Genome assembly', 'Antigen class', 'Antigen', 'Cell type class', 'Cell type'],
-                    usecols=range(6)
+                    sep = ',', 
+                    names = ['id', 'Genome assembly', 'Antigen class', 'Antigen', 'Cell type class', 'Cell type']
+                    #usecols=range(6)
                 )
-    if verbose:
-        print("Find file " +  filepath)
 
     for key in options.keys():
         if options[key]:
@@ -67,12 +62,10 @@ def create_sorted_bed_file(
     df = df.map_partitions(add_sorted_bed_2_file, matching_experiments)
     df.to_csv(path_2_sorted_file, index=False, header=False, single_file=True)
 
-def del_slash(path):
-    return path[:-1] if path[-1] == '/' else path
 
 
-def omics(expid: Union[str,list] = None, assembly: Union[str,list] = 'hg38', assembly_threshold: Union[str,list] = '05' , antigen_class: Union[str,list] = None,
-          antigen: Union[str,list] = None, cell_type: Union[str,list] = None, cell: Union[str,list] = None, storage: Path = 'storage', output_path: Path = './'):
+def omics(expid: list = None, assembly: list = ['hg38'], assembly_threshold: list = '05' , antigen_class: list = None,
+          antigen: list = None, cell_type: list = None, cell: list = None, resources: Path = Path("./loader"), output_path: Path = Path("./storage/")):
     '''
     Function to create omics data from chip-atlas database.
     Arguments:
@@ -93,21 +86,6 @@ def omics(expid: Union[str,list] = None, assembly: Union[str,list] = 'hg38', ass
     Returns:
         No return
     '''
-    
-    storage = del_slash(storage)
-    output_path = del_slash(output_path)
-
-    # move exp file
-    if not os.path.isfile(storage + "/experimentList.tab"):
-        subprocess.run(f"gunzip {storage}/experimentList.tab.gz")
-
-    if not os.path.isfile(storage + f"/allPeaks_light.{assembly}.{assembly_threshold}.bed"):
-        print("new wget")
-        wget.download(f"https://chip-atlas.dbcls.jp/data/hg38/allPeaks_light/allPeaks_light.{assembly}.{assembly_threshold}.bed.gz",
-                      storage
-                    )
-        subprocess.run(f"gunzip {storage}/allPeaks_light.{assembly}.{assembly_threshold}.bed.gz")
-    
     options = {
         "id"                :   expid,
         "Genome assembly"   :   assembly,
@@ -117,18 +95,35 @@ def omics(expid: Union[str,list] = None, assembly: Union[str,list] = 'hg38', ass
         "Cell type"         :   cell
     }
 
+    #check files
+    for a in assembly:
+            SubporocessHub = subprocess.Popen(["python","-W","ignore","./loader/file_creator.py","-a", a, "-t", assembly_threshold, "-c", "1"], stdout= subprocess.PIPE)
+            SubporocessHub.wait()
+            Result = str(SubporocessHub.communicate())
+            
+            if "reload" in Result:
+                print(f"Start reload {a}")
+                #SubporocessHub = subprocess.Popen(["python","-W","ignore","./loader/file_creator.py","-a", a, "-t", assembly_threshold, "-r", "1", "-d", "1"],stdout=subprocess.PIPE)
+                #SubporocessHub.wait()
+            else:
+                print(f"File check of {a} complete")
 
-# Dask multiuser checking.    
-    # with warnings.catch_warnings(record=True) as caught_warnings:
-    #     warnings.simplefilter("always")
-    #     for warn in caught_warnings:
-    #         if str(warn.message).find('Port 8787 is already in use') != -1:
-    #             print(f"{bcolors.OKCYAN}U r not alone. Sorry but u have to w8.\nChill a bit!{bcolors.ENDC}") 
-    #             exit()
 
-    match_exp_df = create_matching_expirement_df(storage + "/experimentList.tab", options)
 
-    create_sorted_bed_file(f"allPeaks_light.{assembly}.{assembly_threshold}.bed", match_exp_df)
+
+    match_exp_df = create_matching_expirement_df(Path("./loader/resources/experimentList.tab"), options)
+    files_string = ''
+    for a in assembly:
+        files_dir = str(resources / f"resources/{a}/")
+        match_exp_l = f'_*,'.join(match_exp_df["id"].loc[match_exp_df["Genome assembly"] == a].to_numpy()) + "_*"
+        files_string+=match_exp_l
+    print(f"Copying {files_string.count('*')}  files")
+        #SubporocessHub = subprocess.Popen("cp ./"+files_dir+'/' + "{" +match_exp_l + "} ./storage/",shell = True)
+    filename_d = strftime("%Y-%m-%d_%H:%M:%S", gmtime())
+    if files_string.count('*') == 1:
+        SubporocessHub = subprocess.Popen(f"tar -czf {output_path}/{filename_d}.tar.gz ./"+files_dir+'/' + match_exp_l,shell = True)
+    else:
+        SubporocessHub = subprocess.Popen(f"tar -czf {output_path}/{filename_d}.tar.gz ./"+files_dir+'/' + "{" +match_exp_l + "}",shell = True)
     
-    os.replace(storage + f"/filtred_allPeaks_light.{assembly}.{assembly_threshold}.bed", output_path)
-    subprocess.run(f"gzip {output_path}/filtred_allPeaks_light.{assembly}.{assembly_threshold}.bed.gz")
+    SubporocessHub.wait()
+    print("Process done")
