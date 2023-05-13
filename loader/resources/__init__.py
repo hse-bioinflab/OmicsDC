68b26c9f6d655dccb2305d63d65793a51cb30594
Paul <borovkov-pavel@yandex.ru>
2023-05-13 18:29:31 +0300

    Improving users experience 	modified:   loader.ipynb 	modified:   loader/resources/__init__.py

--- loader/resources/__init__.py
+++ loader/resources/__init__.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 ROOT = Path(__file__).parent
 
-ExperimentList = Path("loader/resources/experimentList.tab")
+ExperimentList = ROOT.joinpath("./experimentList.tab")
 # Example
 # MY_SUPER_FILE = ROOT.joinpath("my_super_file.txt")
